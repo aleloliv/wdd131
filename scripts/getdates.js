79cbe9b3d2accf9d172415d2a3001eb6562f9405
aleloliv <aleloliv555@hotmail.com>
2024-05-01 09:41:47 -0300

    Weeek01 color correction

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,20 +4,20 @@ const today = new Date();
 
 currentyear.textContent = today.getFullYear();
 
-
 let oLastModif = new Date(document.lastModified);
 
 const lastModified = document.querySelector("#lastModified");
 
-function addLeadingZero(number) {
-    return number < 10 ? '0' + number : number;
-  }
+function addLeadingZero(number) 
+{
+  return number < 10 ? '0' + number : number;
+}
   
-  let formattedDate = addLeadingZero(oLastModif.getMonth() + 1) + '/' +
-                      addLeadingZero(oLastModif.getDate()) + '/' +
-                      oLastModif.getFullYear() + ' ' +
-                      addLeadingZero(oLastModif.getHours()) + ':' +
-                      addLeadingZero(oLastModif.getMinutes()) + ':' +
-                      addLeadingZero(oLastModif.getSeconds());
-  
-  lastModified.textContent = "Last Modification: " + formattedDate;
+let formattedDate = addLeadingZero(oLastModif.getMonth() + 1) + '/' +
+                    addLeadingZero(oLastModif.getDate()) + '/' +
+                    oLastModif.getFullYear() + ' ' +
+                    addLeadingZero(oLastModif.getHours()) + ':' +
+                    addLeadingZero(oLastModif.getMinutes()) + ':' +
+                    addLeadingZero(oLastModif.getSeconds());
+
+lastModified.textContent = "Last Modification: " + formattedDate;
